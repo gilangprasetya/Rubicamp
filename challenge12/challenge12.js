98455072d34fa177063172b7aaf20e2ccd492628
Gilanggg <macbookproo@MacBooks-MacBook-Pro.local>
2023-05-23 14:32:14 +0700

    add c12

---
+++ challenge12/challenge12.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const readline = require('readline');
+const process = require('process')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Jawaban : "
+});
+    let fileName = process.argv[2]
+    if(fileName == 'data.json'){
+        console.log(`Selamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini ${fileName}
+untuk bermain, jawablah dengan jawaban sesuai.
+Gunakan 'skip' untuk menangguhkan pertanyaanya, dan di akhir pertanyaan akan ditanyakan lagi \n`)
+    fs.readFile('data.json', 'utf8', (err, isi) => {
+        if(err) throw err
+        const data = JSON.parse(isi)
+        // console.log(data)
+        let index = 0
+        let salah = 0
+        console.log(`pertanyaan : ${data[index].definition}`)
+        rl.prompt()
+        rl.on('line', (jawab) =>{
+            let jawaban = jawab.toLowerCase()
+            if(jawaban == 'skip'){
+                data.push(data[index])
+                data.splice(index, 1)
+                console.log(`\npertanyaan : ${data[index].definition}`)
+            }else{
+                if(jawaban == data[index].term){
+                    console.log(`\nAnda beruntung!`)
+                    salah = 0
+                    index++
+                    if(index == data.length){
+                        console.log(`\nAnda Berhasil!`)
+                        rl.close()
+                    }else{
+                        console.log(`\npertanyaan : ${data[index].definition}`)
+                    }
+                }else{
+                    salah++
+                    console.log(`\nanda kurang beruntung! anda telah salah ${salah} kali silahkan coba lagi`)
+                }
+            }
+            // if(index == data.length){
+            //     console.log(`Anda Berhasil!`)
+            //     rl.close()
+            // }
+            rl.prompt()
+        }).on('close', () =>{
+            process.exit()
+        })
+    })
+    }else{
+        console.log(`Tolong sertakan nama file sebagai inputannya
+Misalnya : 'node challenge12.js data.json'`)
+        rl.close()
+    }
